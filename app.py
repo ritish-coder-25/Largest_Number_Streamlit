92423a304128ba329678b3586f0a64298c0f66d6
RITISH KUMAR DAS <ritishkr25@gmail.com>
2024-04-21 20:31:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,12 +14,16 @@
 
 import streamlit as st
 
+st.title("Largest Number Finder")
 # +
 num1 = st.number_input("Enter First Number:")
 num2 = st.number_input("Enter Second Number:")
 num3 = st.number_input("Enter Third Number:")
 
 largest = max(num1, num2, num3)
+
+st.markdown("---")  # Creates a horizontal divider
+st.subheader(f"Done by Ritish Kumar Das [21f3000959]")  # Uses f-string for name
 
 st.write("The Largest Number is: ", largest)
 # -
